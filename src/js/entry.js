6cc5b881c358b8f8a5d2612332f40d1a005dcbe5
romuleald <ckkoshi@gmail.com>
2016-12-30 23:34:29 +0100

    merge json & csv to unique file

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,28 +1,41 @@
 import toggler from './core/toggler';
 import webmodule from './core/module';
-import service from './core/get-service';
 import getTpl from './core/getTpl';
 import algoliasearch from 'algoliasearch';
 
 toggler();
 webmodule.init();
-//service.init(jsonendpoint);
-//
-
 
 var client = algoliasearch('TDV4I77F2F', 'c5731b2aa4cb316c0f55990145f0126d');
-var index = client.initIndex('getstarted_actors');
-
+var searchList = client.initIndex('all');
+//need https
+//searchList.setSettings({'hitsPerPage': 200}, function(err) {
+//    if (!err) {
+//        console.log('success');
+//    }
+//});
 $('.js-search').on('input', function (e) {
 
-    index.search(this.value, function searchDone(err, content) {
-        console.log(err, content);
+    searchList.search(this.value, function searchDone(err, content) {
         let html = '';
-        for (let i = 0; i < content.hits.length; i++) {
-            let item = content.hits[i];
+        var hits = content.hits;
+        for (let i = 0; i < hits.length; i++) {
+            let item = hits[i];
             html += getTpl(item, 'tpl_search');
         }
         $('.js-result').html(html);
     });
 
 });
+
+//merge jsons
+//$.getJSON('resources/dataset/restaurants_list.json').done(function (listRes) {
+//    $.getJSON('resources/dataset/restaurants_info.json').done(function (infoRes) {
+//        let all = listRes.map(function(listItem) {
+//            return Object.assign(infoRes.filter(function (infoItem) {
+//                return infoItem.objectID === listItem.objectID;
+//            })[0], listItem)
+//        });
+//        console.info(JSON.stringify(all));
+//    })
+//});
